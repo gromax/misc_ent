2cd36ff1e75c33ae50f85d44ac46ec178dd2e5cc
Maxence Klein <gromaxx@gmail.com>
2019-10-20 17:18:19 +0200

    ajout datepicker

--- dev/initialize.js
+++ dev/initialize.js
@@ -1,7 +1,10 @@
 //import './styles/application.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/jquery-ui/themes/base/all.css';
+import '../node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css';
 import 'bootstrap';
+import '../node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js'
+import '../node_modules/bootstrap-datepicker/dist/locales/bootstrap-datepicker.fr.min.js'
 
 
 document.addEventListener('DOMContentLoaded', () => {
