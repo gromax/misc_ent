ee08a17c2ad490f33a9cb724e9b01ab61ae7cbc7
Maxence Klein <gromaxx@gmail.com>
2019-10-20 17:19:20 +0200

    ajout gestion créneaux d'une offre

--- php/class/RouteController/rendezVousManager.php
+++ php/class/RouteController/rendezVousManager.php
@@ -3,7 +3,8 @@
 namespace RouteController;
 use ErrorController as EC;
 use AuthController as AC;
-use BDDObject\RendezVous;
+use BDDObject\OffreRendezVous;
+use BDDObject\CreneauRendezVous;
 use BDDObject\Droit;
 
 class rendezVousManager
@@ -21,6 +22,29 @@ class rendezVousManager
         $this->params = $params;
     }
 
+    public function fetch()
+    {
+        $ac = new AC();
+        $user = $ac->getloggedUserData();
+        if ($user["type"]=="off")
+        {
+            EC::set_error_code(401);
+            return false;
+        }
+
+        $id = (integer) $this->params['id'];
+        $item=OffreRendezVous::getObject($id);
+        if ($item === null)
+        {
+            EC::set_error_code(404);
+            return false;
+        }
+
+        return array(
+            "offre" => $item->getValues(),
+            "creneaux" => $item->getCreneauxList()
+        );
+    }
 
     public function fetchList()
     {
@@ -31,7 +55,7 @@ class rendezVousManager
             EC::set_error_code(401);
             return false;
         }
-        return RendezVous::getList();
+        return OffreRendezVous::getList();
     }
 
     public function delete()
@@ -46,7 +70,7 @@ class rendezVousManager
         }
 
         $id = (integer) $this->params['id'];
-        $item=RendezVous::getObject($id);
+        $item=OffreRendezVous::getObject($id);
         if ($item === null)
         {
             EC::set_error_code(404);
@@ -54,6 +78,41 @@ class rendezVousManager
         }
 
         if ($item->delete())
+        {
+            return array( "message" => "Model successfully destroyed!");
+        }
+        EC::set_error_code(501);
+        return false;
+    }
+
+    public function deleteCreneau()
+    {
+        $ac = new AC();
+        $user = $ac->getloggedUserData();
+        if ($user["type"]=="off")
+        {
+            EC::set_error_code(401);
+            return false;
+        }
+        $id = (integer) $this->params['id'];
+        $itemCreneau = CreneauRendezVous::getObject($id);
+        if ($itemCreneau === null)
+        {
+            EC::set_error_code(404);
+            return false;
+        }
+        if (!Droit::has($user["login"], 1))
+        {
+            // vérification du droit d'écrire les rendez-vous
+            // Peut-être l'utilisateur est le propriétaire
+            $itemParent = $itemCreneau.getParent();
+            if (($itemParent === null)||!$itemParent->isOwner($user["login"]))
+            {
+                EC::set_error_code(403);
+                return false;
+            }
+        }
+        if ($itemCreneau->delete())
         {
             return array( "message" => "Model successfully destroyed!");
         }
@@ -73,7 +132,7 @@ class rendezVousManager
         }
 
         $data = json_decode(file_get_contents("php://input"),true);
-        $item = new RendezVous();
+        $item = new OffreRendezVous();
         $id = $item->update($data);
         if ($id!==null)
         {
@@ -85,6 +144,47 @@ class rendezVousManager
         return false;
     }
 
+    public function insertCreneau()
+    {
+        $ac = new AC();
+        $user = $ac->getloggedUserData();
+        if ($user["type"]=="off")
+        {
+            EC::set_error_code(401);
+            return false;
+        }
+        $data = json_decode(file_get_contents("php://input"),true);
+        if (!isset($data["idOffre"]))
+        {
+            EC::set_error_code(501);
+            return false;
+        }
+        $idOffre = (integer) $data["idOffre"];
+        if (!Droit::has($user["login"], 1))
+        {
+            // vérification du droit d'écrire les rendez-vous
+            // Peut-être l'utilisateur est le propriétaire
+            $itemParent = OffreRendezVous::getObject($idOffre);
+            if (($itemParent === null)||!$itemParent->isOwner($user["login"]))
+            {
+                EC::set_error_code(403);
+                return false;
+            }
+        }
+        # l'utilisateur a le droit d'ajouter le créneau
+        # Dans un premier temps je ne vérifie pas la cohérence...
+
+        $item = new CreneauRendezVous();
+        $id = $item->update($data);
+        if ($id!==null)
+        {
+            $out = $item->getValues();
+            return $out;
+        }
+
+        EC::set_error_code(501);
+        return false;
+    }
 
 }
 ?>
